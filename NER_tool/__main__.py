094a89661d6f5195691722b565add6c8b9587cbf
Sean Chen <32125983+caterpillar0111@users.noreply.github.com>
2022-05-19 17:44:55 +0800

    add some code and framework

--- NER_tool/__main__.py
+++
@@ -1,8 +0,0 @@
-
-def main():
-    number = int(input('Enter the number (only positive integers allowed\n'))
-    print(f'{number} squares is {number**2}')
-
-
-if __name__ == '__main__':
-    main()
